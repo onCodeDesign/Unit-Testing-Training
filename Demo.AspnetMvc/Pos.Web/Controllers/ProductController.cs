5d59a1357420d78b6a3598ba2e9600230709b7ea
Florin Coros <florin@onCodeDesign.com>
2018-06-25 15:08:16 +0300

    [Asp] 1. When a barcode is scanned show product details

--- Demo.AspnetMvc/Pos.Web/Controllers/ProductController.cs
+++ Demo.AspnetMvc/Pos.Web/Controllers/ProductController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Pos.DataAccess.Model;
+using Pos.DataAccess.Repositories;
+using Pos.Web.Models;
 
 namespace Pos.Web.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IProductRepository repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
         public IActionResult Details(string barcode)
         {
+            string code = barcode.Trim().ToLowerInvariant();
+            Product p = repository.GetProduct(code);
 
-            return View();
+            ProductViewModel vm = new ProductViewModel
+            {
+                Code = p.CatalogCode,
+                Name = p.CatalogName,
+                Price = $"{p.Price} $"
+            };
+
+            return View(vm);
         }
     }
 }
