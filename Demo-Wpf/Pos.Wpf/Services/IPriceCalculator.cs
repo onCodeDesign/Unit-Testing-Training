dfc1aa1030a9e2af206c79d8c2ed3337461e9678
Florin Coros <florin@onCodeDesign.com>
2019-11-13 15:58:01 +0200

    [WPF Demo] Code first, Add PriceCalculator

---
+++ Demo-Wpf/Pos.Wpf/Services/IPriceCalculator.cs
@@ -0,0 +1,9 @@
+﻿using Pos.Wpf.DAL;
+
+namespace Pos.Wpf.Services
+{
+    public interface IPriceCalculator
+    {
+        decimal GetPrice(Product product);
+    }
+}
---
+++ Demo-Wpf/Pos.Wpf/Services/PriceCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Pos.Wpf.DAL;
+
+namespace Pos.Wpf.Services
+{
+    class PriceCalculator : IPriceCalculator
+    {
+        public decimal GetPrice(Product product)
+        {
+            decimal currentPrice = product.Price;
+
+            bool regionalApplied = false;
+            if (product.Taxes.Contains(TaxationType.RegionalTax))
+            {
+                currentPrice = currentPrice + product.Price * 0.1m;
+                regionalApplied = true;
+            }
+
+            if (product.Taxes.Contains(TaxationType.Vat) && !regionalApplied)
+            {
+                currentPrice = currentPrice + product.Price * 0.22m;
+            }
+
+            if (product.Taxes.Contains(TaxationType.LuxuryTax))
+            {
+                currentPrice = currentPrice + product.Price * 0.5m;
+            }
+
+            return currentPrice;
+        }
+    }
+}
