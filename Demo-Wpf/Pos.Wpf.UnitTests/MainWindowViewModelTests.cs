c81c313417a9dc2aa4fa314cff856411e1ff588b
Florin Coros <florin@onCodeDesign.com>
2020-06-09 16:19:53 +0300

    [WPF Demo] Code first, use MyDbContext to go in DB

--- Demo-Wpf/Pos.Wpf.UnitTests/MainWindowViewModelTests.cs
+++ Demo-Wpf/Pos.Wpf.UnitTests/MainWindowViewModelTests.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pos.Wpf.Services;
 
 namespace Pos.Wpf.UnitTests
 {
     [TestClass]
     public class MainWindowViewModelTests
     {
-        
+        [TestMethod]
+        public void BarcodeScanned_ProductExists_ProductCodeDisplayed()
+        {
+            //arrange
+            FakeScanner scanner = new FakeScanner();
+            MainWindowViewModel vm = new MainWindowViewModel(scanner);
+            
+            //act
+            scanner.Scan("some code");
+            
+            // assert
+            Assert.AreEqual("some code", vm.ProductCode);
+        }
+    }
+
+    public class FakeScanner : IScanner
+    {
+        public event EventHandler<BarcodeScannedEventArgs> BarcodeScanned;
+
+        protected virtual void OnBarcodeScanned(BarcodeScannedEventArgs e)
+        {
+            BarcodeScanned?.Invoke(this, e);
+        }
+
+        public void Scan(string someCode)
+        {
+            OnBarcodeScanned(new BarcodeScannedEventArgs(someCode));
+        }
     }
 }
