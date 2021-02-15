211fab778791c60a5aa1600a928d3722ed7029cc
Florin Coros <florin@onCodeDesign.com>
2021-02-15 11:18:05 +0200

    [WPF Demo] Add LuxuryProducts calculation to PriceCalculator

--- Demo-Wpf/Pos.Wpf/DAL/Product.cs
+++ Demo-Wpf/Pos.Wpf/DAL/Product.cs
@@ -13,6 +13,7 @@ namespace Pos.Wpf.DAL
         public string CatalogName { get; set; }
         public decimal Price { get; set; }
         public TaxationType[] Taxes { get; set; }
+        public GoodsCategory Category { get; set; } = GoodsCategory.Convenience;
     }
 
     public enum TaxationType
@@ -20,5 +21,33 @@ namespace Pos.Wpf.DAL
         Vat,
         RegionalTax,
         LuxuryTax
+    }
+
+    public enum GoodsCategory
+    {
+        /// <summary>
+        /// Specialty goods have particularly unique characteristics and brand identifications for which a significant group of buyers is willing to make a special purchasing effort.
+        /// <para>
+        ///     Examples include specific brands of fancy products, luxury cars, professional photographic equipment, and high-fashion clothing.
+        /// </para>
+        /// </summary>
+        Specialty,
+
+        /// <summary>
+        /// Convenience goods are those that are regularly consumed and are readily available for purchase.
+        /// <para>
+        ///     These goods are mostly sold by wholesalers and retailers and include items such as milk and tobacco products.
+        /// </para>
+        /// </summary>
+        Convenience,
+
+        /// <summary>
+        /// Shopping goods are those in which a purchase requires more thought and planning than with convenience goods
+        /// <para>
+        ///     Shopping goods are more expensive and have more durability and longer lifespans than convenience goods
+        ///     Shopping goods include furniture and televisions.
+        /// </para>
+        /// </summary>
+        Shopping
     }
 }
