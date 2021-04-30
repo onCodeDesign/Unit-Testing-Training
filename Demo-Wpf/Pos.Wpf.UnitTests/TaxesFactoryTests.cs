63ef0759ee313a8acdf3fdf1a11b4b7767b0ee77
Florin Coros <florin@onCodeDesign.com>
2021-04-30 11:13:56 +0300

    [WPF Demo] Add more tests to TaxesFactoryTests

--- Demo-Wpf/Pos.Wpf.UnitTests/TaxesFactoryTests.cs
+++ Demo-Wpf/Pos.Wpf.UnitTests/TaxesFactoryTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using iQuarc.xUnitEx;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pos.Wpf.DAL;
@@ -32,6 +34,29 @@ namespace Pos.Wpf.UnitTests
             var actualTaxesTypes = actualTaxes.Select(x => x.GetType());
             AssertEx.AreEquivalent(actualTaxesTypes, typeof(RegionalTax));
 
+        }
+
+        [TestMethod]
+        public void GetTaxes_ForProductWithNoTaxes_NoTaxesReturned()
+        {
+            TaxesFactory target = GetTarget();
+            Product product = new Product();
+
+            var actualTaxes = target.GetTaxesFor(product);
+
+            Assert.AreEqual(0, actualTaxes.Count());
+        }
+
+        [TestMethod]
+        public void GetTaxes_ForProductWithAllTaxes_RegionalAndLuxuryReturned()
+        {
+            var target = GetTarget();
+            Product product = new Product{Taxes = new [] {TaxationType.Vat, TaxationType.RegionalTax, TaxationType.LuxuryTax}};
+
+            var actualTaxes = target.GetTaxesFor(product);
+
+            IEnumerable<Type> actualTaxesTypes = actualTaxes.Select(x => x.GetType());
+            AssertEx.AreEquivalent(actualTaxesTypes, typeof(RegionalTax), typeof(LuxuryTax));
         }
 
         private TaxesFactory GetTarget()
